5653c6e41338c9025471401b82c71d715bcad4ee
Gayatri Gurav <gayatri.gurav86@gmail.com>
2024-04-02 19:18:35 -0500

    Added create blogPost API. Added Controlller and DTO for create BlogPost. Added Repository interface and implementation. Added constructor dependency injection of DbContext in the repository. Added scope in the program file and then added constructor dependency injection of repository to the controller.

---
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,64 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+
+
+        //POST: {apibaseurl}/api/blogposts
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            // Covert DTO to domain model
+
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                publishedDate = request.publishedDate,
+                ShortDescription = request.ShortDescription,
+                Title = request.Title,
+                UrlHandle = request.UrlHandle,
+
+            };
+
+         blogPost  = await blogPostRepository.createAsync(blogPost);
+
+            //Convert Domain Model to DTO
+
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                publishedDate = blogPost.publishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle
+            };
+
+            return Ok(response);
+
+
+        }
+        
+    }
+}
